218560d6f556620c67a3b824d51c7ec9d3feda5f
PATamparong <tamparongpreciousangel@gmail.com>
2019-10-04 21:49:14 +0800

    Comment

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Setup basic express server
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('../..')(server);
 var port = process.env.PORT || 3001;
 
 server.listen(port, () => {
